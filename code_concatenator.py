d4592beb331d663baaf0f5aed218a4f25eee0916
Sum1Solutions <17915475+Sum1Solutions@users.noreply.github.com>
2023-07-13 09:17:16 -0400

    Your branch is ahead of 'origin/master' by 5 commits.

--- code_concatenator.py
+++ code_concatenator.py
@@ -2,7 +2,7 @@ import os
 import fnmatch
 
 # File types to be collected
-include_file_types = ['*.py', '*.txt']
+include_file_types = ['*.py', '*.txt','*.html', '*.json','*.css', '*.js']
 
 # File types and directories to be excluded
 exclude_file_types = ['*.zip', '*.pyc', '*.log',
@@ -11,33 +11,38 @@ exclude_file_types = ['*.zip', '*.pyc', '*.log',
                       'README.md',
                       'LICENSE.txt']
 
-
 exclude_dirs = ['env', 
                 '.git', 
                 '.vscode', 
                 '__pycache__']
 
-def print_dir_tree(directory, output_file, level=0):
-    indent = ' ' * 4 * level
-    for item in os.listdir(directory):
-        item_path = os.path.join(directory, item)
-        if os.path.isdir(item_path) and item not in exclude_dirs:
-            output_file.write(f"{indent}Directory: {item}\n")
-            print_dir_tree(item_path, output_file, level + 1)
+def print_dir_tree(directory, output_file, level=-1):
+    indent = '|  ' * level
+    try:
+        for item in os.listdir(directory):
+            item_path = os.path.join(directory, item)
+            if os.path.isdir(item_path) and item not in exclude_dirs:
+                output_file.write(f"{indent}|- {item}\n")
+                print_dir_tree(item_path, output_file, level + 1)
+    except Exception as e:
+        output_file.write(f"Error while listing directory {directory}: {e}\n")
 
 def print_files(directory, output_file):
-    for root, dirs, files in os.walk(directory):
-        dirs[:] = [d for d in dirs if d not in exclude_dirs]
-        for file in files:
-            if any(fnmatch.fnmatch(file, pattern) for pattern in include_file_types) and not any(fnmatch.fnmatch(file, pattern) for pattern in exclude_file_types):
-                file_path = os.path.join(root, file)
-                output_file.write(f"\nFile: {file_path}\n")
-                try:
-                    with open(file_path, "r") as file_to_include:
-                        output_file.write(file_to_include.read())
-                        output_file.write("\n")
-                except Exception as e:
-                    output_file.write(f"Error reading file: {e}\n")
+    try:
+        for root, dirs, files in os.walk(directory):
+            dirs[:] = [d for d in dirs if d not in exclude_dirs]
+            for file in files:
+                if any(fnmatch.fnmatch(file, pattern) for pattern in include_file_types) and not any(fnmatch.fnmatch(file, pattern) for pattern in exclude_file_types):
+                    file_path = os.path.join(root, file)
+                    output_file.write(f"\nFile: {file_path}\n")
+                    try:
+                        with open(file_path, "r") as file_to_include:
+                            output_file.write(file_to_include.read())
+                            output_file.write("\n")
+                    except Exception as e:
+                        output_file.write(f"Error reading file {file_path}: {e}\n")
+    except Exception as e:
+        output_file.write(f"Error while walking directory {directory}: {e}\n")
 
 # Directory to be collected
 directory = "."
